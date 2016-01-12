a506f5ff9331c365630810ca20dacbdf76ac856d
crlang44 <crlang44@gmail.com>
2016-01-11 18:32:57 -0600

    Removed unnecessary build folder

--- build/lib/cc_counter/lizard_ext/__init__.py
+++
@@ -1,7 +0,0 @@
-''' extensions of lizard '''
-
-from .javascript import JavaScriptReader
-from .python import PythonReader
-from .objc import ObjCReader
-from .ttcn import TTCNReader
-from .xmloutput import xml_output
